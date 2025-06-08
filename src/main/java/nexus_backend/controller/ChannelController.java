99f04271cc8a8641a9be2b23d6a9f2cd6bd5195e
cgcm070 <cesar.gabriel.martinezs7@gmail.com>
2025-06-08 15:45:06 +0200

    estilo de los campos del formulario de autenticación

--- src/main/java/nexus_backend/controller/ChannelController.java
+++ src/main/java/nexus_backend/controller/ChannelController.java
@@ -35,7 +35,6 @@ public class ChannelController {
         return channelService.getChannelById(id);
     }
 
-    @PreAuthorize("@securityService.canManageChannel(#channelDTO.serverId)")
     @PostMapping("")
     public Channel createChannel(@RequestBody ChannelCreateDTO channelDTO) {
         log.info("Creating new channel for server {}", channelDTO.getServerId());
